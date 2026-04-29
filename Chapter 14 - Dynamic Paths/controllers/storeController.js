bf761fc9d77017b038191b7c2e3299089446b13b
Muhammad Arslan <imarslan7061@gmail.com>
2026-04-30 03:59:10 +0500

    update
    working on Dynamic Paths

---
+++ Chapter 14 - Dynamic Paths/controllers/storeController.js
@@ -0,0 +1,56 @@
+const Home = require("../models/home");
+
+exports.getIndex = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+    }),
+  );
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Homes List",
+      currentPage: "Home",
+    }),
+  );
+};
+
+exports.getBookings = (req, res, next) => {
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  });
+};
+
+exports.getFavouriteList = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/favourite-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "My Favourites",
+      currentPage: "favourites",
+    }),
+  );
+};
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId, (home) => {
+    if (!home) {
+      console.log("Home Not Found");
+      res.redirect("/homes");
+    } else {
+      console.log("Home Details Found", home);
+      res.render("store/home-detail", {
+        homeId,
+        home,
+        pageTitle: "Home Details",
+        currentPage: "Home",
+      });
+    }
+  });
+};
